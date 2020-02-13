fe256a299c67a15cdf333c277b549cc5bd22153f
avalentini <antonio.valentini@live.it>
2020-02-13 15:29:24 +0100

    Add UnitTest project and start aesgcm analysis

---
+++ src/Userbase.Client.UnitTests/Crypto/AesGcmUtilsTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Userbase.Client.Crypto;
+using Xunit;
+
+namespace Userbase.Client.UnitTests.Crypto
+{
+    public class AesGcmUtilsTests
+    {
+        [Fact]
+        public void HashesShouldMatch()
+        {
+            // ARRANGE
+            const string correctHash = "";
+            byte[] passwordKeyHash = null;
+            var passwordBasedEncryptionKeySalt = Convert.FromBase64String("");
+            var aesGcm = new AesGcmUtils();
+
+            // ACT
+            var resultHash = aesGcm.GetSeedStringFromPasswordBasedBackup(passwordKeyHash, passwordBasedEncryptionKeySalt);
+
+            // ASSERT
+            Assert.Equal(correctHash, resultHash);
+        }
+    }
+}
---
+++ src/Userbase.Client/Crypto/AesGcmUtils.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Userbase.Client.Crypto
+{
+    public class AesGcmUtils
+    {
+        public string GetSeedStringFromPasswordBasedBackup(byte[] passwordKeyHash, byte[] passwordBasedEncryptionKeySalt)
+        {
+            byte[] ciphertext = null;
+            byte[] tag = null;
+            var plaintext = Utils.FillOddsWithZeros(Encoding.ASCII.GetBytes("password-based-encryption"));
+
+            var aesgcm = new AesGcm(passwordBasedEncryptionKeySalt);
+            aesgcm.Encrypt(passwordKeyHash, plaintext, ciphertext, tag);
+
+            var seedStringFromBackup = Convert.ToBase64String(ciphertext);
+
+            return seedStringFromBackup;
+        }
+    }
+}
